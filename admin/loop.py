dfb20d0f8e400fe0353cceec722bf5909b6629e0
Allen Downey <downey@allendowney.com>
2013-10-07 14:29:43 -0400

    Adding loop.py

---
+++ admin/loop.py
@@ -0,0 +1,60 @@
+import subprocess
+
+repos = [
+    'bunyipc',
+    'juliebarron',
+    'tchoetso',
+    'acrease77',
+    'zfiddler',
+    'adgarrett',
+    'mgiolando',
+    'cg123',
+    'AmbikaGoel',
+    'eguthrie',
+    'hhansson',
+    'skumarasena',
+    'JLangowitz',
+    'mmahlenk',
+    'amuthaOlin',
+    'allisongpatterson',
+    'JPWS2013',
+    'vpreston',
+    'ETumang',
+    'awilkinson88',
+    'Brooks-Willis',
+    'morganzhu',
+]
+
+def run_command(cmd):
+    """Runs a command in a subprocess.
+
+    cmd: string
+    """
+    popen = subprocess.Popen(cmd, shell=True)
+    res = popen.communicate()
+    return res
+
+
+def clone():
+    for repo in repos:
+        s = 'git clone https://github.com/%s/SoftwareDesign.git %s'
+        cmd = s % (repo, repo)
+        print cmd
+        run_command(cmd)
+
+
+def run_python(repo, dirname, filename):
+    s = 'cd %s/%s; python %s'
+    cmd = s % (repo, dirname, filename)
+    print cmd
+    out, err = run_command(cmd)
+    print out
+    print err
+
+
+def hw3():
+    for repo in repos:
+        run_python(repo, 'chap03', 'grid.py')
+        run_python(repo, 'chap04', 'polygon.py')
+
+hw3()
